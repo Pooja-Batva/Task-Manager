6fba9c571392fc7c0e76bfe400ef643c9ed0f1bc
Pooja-Batva <poojabatva2602@gmail.com>
2026-07-14 16:35:53 +0530

    feat: add cors for allow all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
 import taskRoutes from "./routes/task.route.js";
 import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: true, // allow all origins (adjust in production)
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
