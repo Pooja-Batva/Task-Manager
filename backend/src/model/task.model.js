08238f07cc2b3e9775cbc2de01e197239e1711ab
Pooja-Batva <poojabatva2602@gmail.com>
2026-07-14 14:52:15 +0530

    feat: add task model with schema for title, description, status, priority, and due date

---
+++ backend/src/model/task.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String
+        },
+        status: {
+            type: String,
+            enum: ["todo", "in progress", "done"],
+            default: "todo"
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            default: "low"
+        },
+        dueDate: {
+            type: Date,
+            default: Date.now(),
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Task = mongoose.model("Task", taskSchema);
